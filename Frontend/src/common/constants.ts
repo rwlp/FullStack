ace1c3fb77b2cd193afce32e3b752276b2323e47
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-08-06 18:16:11 -0300

    update baseurl_api

--- Frontend/src/common/constants.ts
+++ Frontend/src/common/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL_CDN = "https://rwlp.github.io/FullStack_CDN";
-export const BASE_URL_API = "http://localhost:3000/api";
+export const BASE_URL_API = "https://fullstack-2485.onrender.com/api";
